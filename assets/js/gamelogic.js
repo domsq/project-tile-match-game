70551779fe471e699592d763f3c705f1dd2d811d
domsq <Domsq@hotmail.com>
2021-06-16 14:23:38 +0000

    Upload tile images and create array

---
+++ assets/js/gamelogic.js
@@ -0,0 +1,68 @@
+// Create array to hold tile values
+
+const tiles = [
+    {
+        name: 'blue flower',
+        path: 'assets/images/blue-flower-gabriel-manlake.jpg'
+    },
+    {
+        name: 'blue flower',
+        path: 'assets/images/blue-flower-gabriel-manlake.jpg'
+    },
+    {
+        name: 'daisy',
+        path: 'assets/images/daisy-flower-marissa-daeger.jpg'
+    },
+    {
+        name: 'daisy',
+        path: 'assets/images/daisy-flower-marissa-daeger.jpg'
+    },
+    {
+        name: 'dark pink flower',
+        path: 'assets/images/dark-pink-flower-tra-tran.jpg'
+    },
+    {
+        name: 'dark pink flower',
+        path: 'assets/images/dark-pink-flower-tra-tran.jpg'
+    },
+    {
+        name: 'light purple flower',
+        path: 'assets/images/light-purple-flower-karl-heinz-muller.jpg'
+    },
+    {
+        name: 'light purple flower',
+        path: 'assets/images/light-purple-flower-karl-heinz-muller.jpg'
+    },
+    {
+        name: 'pink flower',
+        path: 'assets/images/pink-flower-achim-ruhnau.jpg'
+    },
+    {
+        name: 'pink flower',
+        path: 'assets/images/pink-flower-achim-ruhnau.jpg'
+    },
+    {
+        name: 'purple flower',
+        path: 'assets/images/purple-flower-frank-busch.jpg'
+    },
+    {
+        name: 'purple flower',
+        path: 'assets/images/purple-flower-frank-busch.jpg'
+    },
+    {
+        name: 'red flower',
+        path: 'assets/images/red-flower-mana5280.jpg'
+    },
+    {
+        name: 'red flower',
+        path: 'assets/images/red-flower-mana5280.jpg'
+    },
+    {
+        name: 'white flower',
+        path: 'assets/images/white-pink-flower-angelika-paduch.jpg'
+    },
+    {
+        name: 'white flower',
+        path: 'assets/images/white-pink-flower-angelika-paduch.jpg'
+    }
+]
